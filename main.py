a1577c5f644166bf8921b9df0d4a6c4b49ec0468
dmm2017 <meleshko.dmitrii@gmail.com>
2019-02-09 18:25:50 -0500

    Bunch of updates

--- main.py
+++ main.py
@@ -1,77 +1,101 @@
 import kivy
 kivy.require('1.10.1')
 
-
+import time
 from kivy.app import App
+from kivy.lang import Builder
+import os.path
+from kivy.storage.jsonstore import JsonStore
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-
+from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from plyer import accelerometer
 from kivy.uix.textinput import TextInput
 
-class InterfaceManager(BoxLayout):
 
+class WelcomeScreen(Screen):
+    pass
+
+class MainScreen(Screen):
+    pass
+
+class MotorScreen(Screen):
     def __init__(self, **kwargs):
-        super(InterfaceManager, self).__init__(**kwargs)
-        self.forms = {}
+        super(MotorScreen, self).__init__(**kwargs)
+        self.sensorEnabled = False
+        self.values = []
+    def do_toggle(self):
+        try:
+            if not self.sensorEnabled:
+                accelerometer.enable()
+                Clock.schedule_interval(self.get_acceleration, 1 / 20.)
+                self.sensorEnabled = True
+                self.ids.toggle_button.disabled = True
+                time.sleep(5)
+                accelerometer.disable()
+                Clock.unschedule(self.get_acceleration)
+                self.sensorEnabled = False
+                self.ids.toggle_button.disabled = False
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            status = "Accelerometer is not implemented for your platform"
+            self.ids.accel_status.text = status
 
-        self.btn1 = Button(text='Touch screen to begin')
-        self.btn1.bind(on_press=self.uniCallback)
+    def get_acceleration(self, dt):
+        val = accelerometer.acceleration[:3]
 
-        self.btn2 = Button(text='Speech function')
-        self.btn2.bind(on_press=self.uniCallback)
+        if not val == (None, None, None):
+            self.ids.x_label.text = "X: " + str(val[0])
+            self.ids.y_label.text = "Y: " + str(val[1])
+            self.ids.z_label.text = "Z: " + str(val[2])
 
-        self.btn3 = Button(text='Motor function')
-        self.btn3.bind(on_press=self.uniCallback)
+class SpeechScreen(Screen):
+    pass
 
-        self.btn4 = Button(text='Face recognition')
-        self.btn4.bind(on_press=self.uniCallback)
+class ImageScreen(Screen):
+    pass
 
-        self.lbl5 = Label(text='Here goes motor funtion')
-        self.lbl5.bind(on_hover=self.uniCallback)
-
-
-        self.add_widget(self.btn1)
-        self.add_form("Speech function", self.btn1)
-        self.add_form("Motor function", self.lbl5)
-        self.add_form("Face recognition", self.btn1)
-
-        self.add_form("Touch screen to begin", self.btn2)
-        self.add_form("Touch screen to begin", self.btn3)
-        self.add_form("Touch screen to begin", self.btn4)
-
-        self.add_form("Here goes motor funtion", self.btn2)
-        self.add_form("Here goes motor funtion", self.btn3)
-        self.add_form("Here goes motor funtion", self.btn4)
-
-
-    def add_form(self, key, form):
-        if key not in self.forms:
-            self.forms[key] = []
-        self.forms[key].append(form)
-
-    def uniCallback(self, button):
-        self.clear_widgets()
-        for widget in self.forms[button.text]:
-            self.add_widget(widget)
+class BasicEvalScreen(Screen):
+    pass
 
 
-class LoginScreen(GridLayout):
+class FillAbleTextInput(TextInput):
+    def set_values_from_config(self):
+        self.config_name = "obj/config.json"
+        if os.path.isfile(self.config_name):
+            config = JsonStore(self.config_name)
+            if config.exists("user"):
+                self.text = config.get('user')[self.hint_text.lower()]
 
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 1
-        self.add_widget(Button(text='Motor Function'))
-        self.add_widget(Button(text='Face Function'))
-        self.add_widget(Button(text='Voice Function'))
+
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("main.kv")
+
 
 class MyApp(App):
 
+    def __init__(self):
+        App.__init__(self)
+        self.config_name = "obj/config.json"
+
+
+    def create_configuration(self):
+        config = JsonStore(self.config_name)
+        name = self.root.ids["welcome"].ids["name"].text
+        lastname = self.root.ids["welcome"].ids["lastname"].text
+        emergency = self.root.ids["welcome"].ids["emergency"].text
+        config.put('user', name = name, lastname = lastname, emergency = emergency)
+
     def build(self):
-        manager = InterfaceManager(orientation='vertical')
-        return manager
+        return presentation
+
 
 if __name__ == '__main__':
     MyApp().run()
