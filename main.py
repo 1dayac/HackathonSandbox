4a17fab4ded75008b1ed211b8c029f3ff42e0170
dmm2017 <meleshko.dmitrii@gmail.com>
2019-02-09 21:12:10 -0500

    Try doing apk

--- main.py
+++ main.py
@@ -10,11 +10,63 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from plyer import accelerometer
 from kivy.uix.textinput import TextInput
+from kivy.properties import StringProperty
+import speech_recognition as sr
+from multiprocessing import Pool
+
+
+r = sr.Recognizer()
+m = sr.Microphone()
+
+
+class RecordButton(Button):
+    # String Property to Hold output for publishing by Textinput
+    output = StringProperty('')
+    img = StringProperty('')
+
+    def recognize(self, answer, dt):
+        with m as source:
+            audio = r.listen(source, phrase_time_limit=3)
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio, show_all = True)
+            guesses = [d['transcript'] for d in value['alternative']]
+            if answer in guesses:
+                self.output = "Correct! You said \"{}\" ".format(answer)
+            else:
+                self.output = "Incorrect! You said \"{}\" \n \"{}\"".format(guesses[0], guesses)
+
+        except sr.UnknownValueError:
+            self.output = ("Oops! Didn't catch that")
+
+        except sr.RequestError as e:
+            self.output = ("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+        except TypeError:
+            self.output = ("Uh oh! Try again!")
+
+
+    def reload(self, figure):
+        self.parent.parent.ids.img1.source = figure
+        self.parent.parent.ids.img1.reload()
+
+    def record(self):
+
+        config = JsonStore("obj/speech.json")
+        d = list(config.get('speech').items())
+        import random
+        ran = random.randint(0,4)
+        answer = d[ran][0]
+        figure = d[ran][1]
+        self.reload(figure)
+        from functools import partial
+        Clock.schedule_once(partial(self.recognize, answer), 1)
+
 
 
 class WelcomeScreen(Screen):
@@ -27,7 +79,16 @@ class MotorScreen(Screen):
     def __init__(self, **kwargs):
         super(MotorScreen, self).__init__(**kwargs)
         self.sensorEnabled = False
-        self.values = []
+        self.x_values = []
+        self.y_values = []
+        self.z_values = []
+
+    def analyze_results(self):
+        self.x_values = []
+        self.y_values = []
+        self.z_values = []
+
+
     def do_toggle(self):
         try:
             if not self.sensorEnabled:
@@ -40,6 +101,7 @@ class MotorScreen(Screen):
                 Clock.unschedule(self.get_acceleration)
                 self.sensorEnabled = False
                 self.ids.toggle_button.disabled = False
+                self.analyze_results()
         except NotImplementedError:
             import traceback
             traceback.print_exc()
@@ -53,6 +115,9 @@ class MotorScreen(Screen):
             self.ids.x_label.text = "X: " + str(val[0])
             self.ids.y_label.text = "Y: " + str(val[1])
             self.ids.z_label.text = "Z: " + str(val[2])
+            self.x_values.append(val[0])
+            self.y_values.append(val[1])
+            self.z_values.append(val[2])
 
 class SpeechScreen(Screen):
     pass
@@ -70,8 +135,10 @@ class FillAbleTextInput(TextInput):
         if os.path.isfile(self.config_name):
             config = JsonStore(self.config_name)
             if config.exists("user"):
-                self.text = config.get('user')[self.hint_text.lower()]
-
+                try:
+                    self.text = config.get('user')[self.hint_text.lower()]
+                except:
+                    pass
 
 class ScreenManagement(ScreenManager):
     pass
